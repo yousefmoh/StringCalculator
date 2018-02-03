2b7ef2c319ad0f9bbc424d5be64ce64e262af927
YousefMoh7 <yousefmoh7@gmail.com>
2018-02-03 23:05:48 +0200

    handle unkownn amount of numbers and newlines

--- StringCalculator/Calculator.cs
+++ StringCalculator/Calculator.cs
@@ -13,8 +13,53 @@ namespace StringCalculator
         public int Add(string numbers)
         {
             int result = 0;
-           
-            
+            string[] delimiters = { };
+
+
+
+            //Empty String 
+            if (string.IsNullOrEmpty(numbers))
+            {
+                result = 0;
+            }
+            //Length of string equals 1
+            else if (numbers.Length == 1)
+            {
+                result = int.Parse(numbers);
+            }
+
+            //Invalid Input
+            else if (numbers.Contains(",\n"))
+            {
+                throw new Exception("Invalid Input " );
+
+            }
+
+            //handle an unknown amount of numbers without new line 
+            else if (numbers.Length > 1 && !numbers.Contains("\n"))
+            {
+                delimiters = numbers.Split(',');
+            }
+            else
+            {
+                delimiters = numbers.Split(new string[] { ",", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            }
+
+            if (delimiters.Length > 0)
+            {
+
+                foreach (string number in delimiters)
+                {
+                    result = int.Parse(number) + result;
+                }
+            }
+
+
+
+
+
+
             return result;
 
 
